07ce6f327cbb392b9f6436f94a62e9dfb87088b3
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-10-09 17:30:52 +0200

    initial commit2

---
+++ Rasterizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rasterizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
